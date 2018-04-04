6fd275168851cb8663470c8733ff6bd741f2e58c
fjc0k <fjc0kb@gmail.com>
2018-04-04 20:09:41 +0800

    chore

---
+++ packages/@fir-ui/fir-bundler/test/dist/a.cjs.js
@@ -0,0 +1,14 @@
+/*!
+ * @fir-ui/bundler v0.4.0
+ * (c) 2018-present fjc0k
+ * Released under the MIT License.
+ */
+'use strict';
+
+var styles = {"x":"#00f","button":"style-module_button__3F_9I","primary":"style-module_primary__WOaTk","disabled":"style-module_disabled__3zAOh"};
+
+var a = {
+  styles: styles
+};
+
+module.exports = a;
