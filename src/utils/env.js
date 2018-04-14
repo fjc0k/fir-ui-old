41b6b29887fab7c0834566a034197b92442ce201
fjc0k <fjc0kb@gmail.com>
2018-04-15 06:16:13 +0800

    feat: ensure browser

--- src/utils/env.js
+++ src/utils/env.js
@@ -1,1 +1,9 @@
 export const inBrowser = typeof window !== 'undefined'
+
+export function ensureBrowser(resolve, reject) {
+  if (inBrowser) {
+    resolve(window)
+  } else {
+    reject && reject()
+  }
+}
